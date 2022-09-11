d8fb7f0d6c42b1af971998296faba6012b7b1818
Christopher <34822892+ChristopherDeveloper@users.noreply.github.com>
2022-09-11 16:54:56 +0000

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import VueMathjax from 'vue-mathjax-next';
-
-createApp(App).use(router).use(VueMathjax).mount('#app')
