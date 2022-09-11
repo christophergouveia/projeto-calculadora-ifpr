54018faf1aae5b25a9623c295f972248cf40fafb
Christopher <34822892+ChristopherDeveloper@users.noreply.github.com>
2022-09-11 16:54:48 +0000

    Delete public directory

--- public/navbar.js
+++
@@ -1,7 +0,0 @@
-fetch('./navbar.txt')
-  .then(response => response.text())
-  .then(text => {
-    let elementoPai = document.getElementsByClassName("navbar-buttons");
-    console.log(elementoPai);
-    elementoPai.innerHTML = text;
-  })
